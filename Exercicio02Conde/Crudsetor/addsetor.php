324c7291d71d55432d2b770bc238bd196a9ee880
JP-COnde5 <jcgalves25@hotmail.com>
2024-03-12 17:17:21 -0300

    Inserts inseridos

---
+++ Exercicio02Conde/Crudsetor/addsetor.php
@@ -0,0 +1,21 @@
+<?php
+    require_once("../BD/init.php");
+    $nome = isset($_POST["nome"]) ? $_POST["nome"] : null;
+    $despesa = isset($_POST["despesa"]) ? $_POST : null;
+    if(empty($nome)){
+        echo "Preencha todos os campos!!!";
+        exit;
+    }
+    $PDO = db_connection();
+    $cod = "INSERT INTO setor(nome, despesa) VALUES(:nome, :despesa)";
+    $exe = $PDO->prepare($PDO);
+    $exe->bindParam(":nome", $nome);
+    $exe->bindParam(":despesa", $despesa);
+
+    if($exe->execute()){
+        header("Location: ../Index/index.html");
+    }else{
+        echo "Erro ao inserir";
+        print_r($exe->erroInfo());
+    }
+?>
